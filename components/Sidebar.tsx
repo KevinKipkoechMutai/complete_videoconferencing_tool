8a4130314ba04e49210e23b36e3fa55e046b73ff
Kevin Kipkoech <mutaikevinkip.05@gmail.com>
2024-05-17 13:31:04 +0300

    update icons

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -5,6 +5,7 @@ import { sideBarLinks } from '@/app/constants'
 import { cn } from '@/lib/utils'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
+import Image from 'next/image'
 
 const Sidebar = () => {
     const pathname = usePathname()
@@ -15,10 +16,13 @@ const Sidebar = () => {
                 const isActive = pathname === link.route || pathname.startsWith(link.route)
             return (
                 <Link href={link.route} key={link.label} className={cn('flex gap-4 items-center p-4 rounded-lg justify-start', { 'bg-dark-1': isActive })}>
-                    {link.label}
+                    <Image src={link.imgUrl} alt={link.label} width={24} height={24}/>
+                    <p className='text-lg font-semibold max-lg:hidden'>
+                        {link.label}
+                    </p>
                 </Link>
             )
-        })}
+            })}
         </div>
     </section>
   )
