87c3aae6dc91f711dccc3d2b1a4deeeadc765cdb
Kevin Kipkoech <mutaikevinkip.05@gmail.com>
2024-05-20 22:50:35 +0300

    complete home cards and meeting setup

---
+++ components/MeetingTypeList.tsx
@@ -0,0 +1,67 @@
+"use client"
+
+import Image from 'next/image'
+import React, { useState } from 'react'
+import HomeCard from './HomeCard'
+import { useRouter } from 'next/navigation'
+import MeetingModal from './MeetingModal'
+
+const MeetingTypeList = () => {
+
+    const router = useRouter()
+
+    const [meetingState, setMeetingState] = 
+        useState<'isScheduleMeeting' | 'isJoiningMeeting' | 'isInstantMeeting' | undefined>()
+    
+    const createMeeting = () => {
+        
+    }
+
+  return (
+    <section className='grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4'>
+        <HomeCard 
+            img='/icons/add-meeting.svg'
+            title='New Meeting'
+            description='Start an instant meeting'
+            color='bg-orange-1'
+            handleClick={() => setMeetingState('isInstantMeeting')}
+
+        />
+        <HomeCard 
+            img='/icons/schedule.svg'
+            title='Schedule Meeting'
+            description='Plan your meeting'
+            color='bg-blue-1'
+            handleClick={() => setMeetingState('isScheduleMeeting')}
+
+        />
+        <HomeCard 
+            img='/icons/recordings.svg'
+            title='View Recordings'
+            description='Check out your recordings'
+            color='bg-purple-1'
+            handleClick={() => router.push('/recordings')}
+
+        />
+        <HomeCard 
+            img='/icons/join-meeting.svg'
+            title='Join Meeting'
+            description='via invitation link'
+            color='bg-yellow-1'
+            handleClick={() => setMeetingState('isJoiningMeeting')}
+
+        />
+
+        <MeetingModal 
+            isOpen={meetingState === 'isInstantMeeting'}
+            onClose={() => setMeetingState(undefined)}
+            title="Start an instant meeting"
+            className="text-center"
+            buttonText="Start Meeting"
+            handleClick={createMeeting}
+        />
+    </section>
+  )
+}
+
+export default MeetingTypeList
